50c58eb1ec9b2c9b0a2287f6f8dc239f04b98077
liwei <liweili@qq.com>
2021-12-05 13:17:11 +0100

    Intermedia3Korigieren

--- src/main/java/Intermediate3.java
+++ src/main/java/Intermediate3.java
@@ -2,12 +2,13 @@ public class Intermediate3 {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0) {
-                System.out.println("#");
-            } else if (i % 5 == 0) {
-                System.out.println("$");
-            } else {
-                System.out.println(i);
+                System.out.print("#");
             }
+            if (i % 5 == 0) {
+                System.out.print("$");
+            }
+            System.out.print(i);
+            System.out.println();
         }
     }
 }
